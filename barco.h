24b05cff29ec36cec7bbe2a0e9fa665885b1d6c2
churruscat <luis.morras@gmail.com>
2021-09-02 13:19:07 +0200

    Update barco.h

--- barco.h
+++ barco.h
@@ -6,6 +6,6 @@
 #define LOCATION "Barco"
 #define DEVICE_ID "AmperioPrueba"
 #define TOKEN "Token-de-Barco"
-#define INTERVALO_AMPERIO 58000 // 1 min => *** Must be higher than INTERVALO_RPM***
+#define INTERVALO_AMPERIO 58000 // *** INTERVALO_RPM < INTERVALO_AMPERIO < 99*INTERVALO_RPM
 #define INTERVALO_RPM 5000 // 5 seg
 #define INTERVALO_CONEXION 30000
